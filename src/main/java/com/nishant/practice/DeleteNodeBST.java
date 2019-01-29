09d72e150e6e29e543cd29ea82c7cc579f8fd547
nishantbhb <nishantbhb@gmail.com>
2019-01-29 22:13:33 +0530

    delete node in BST

---
+++ src/main/java/com/nishant/practice/DeleteNodeBST.java
@@ -0,0 +1,65 @@
+package com.nishant.practice;
+
+class TreeBuilder {
+    TreeNode buildTree() {
+        TreeNode treeNode = new TreeNode(5);
+        treeNode.left = new TreeNode(3);
+        treeNode.left.left = new TreeNode(2);
+        treeNode.left.right = new TreeNode(4);
+        treeNode.right = new TreeNode(6);
+        treeNode.right.right = new TreeNode(7);
+        return treeNode;
+    }
+
+    TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null)
+            return null;
+
+        if (root.val == key) {
+            if (root.left == null && root.right == null)
+                root = null;
+            else if (root.left == null)
+                root = root.right;
+            else if (root.right == null)
+                root = root.left;
+            else {
+                System.out.println("here");
+                TreeNode curr = root.right;
+                while (curr.left != null)
+                    curr = curr.left;
+                root.val = curr.val;
+                if (curr.right != null)
+                    curr = curr.right;
+                else
+                    curr = null;
+
+                TreeNode curr1 = curr;
+            }
+
+        } else if (root.val > key)
+            root.left = deleteNode(root.left, key);
+        else
+            root.right = deleteNode(root.right, key);
+
+        return root;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
+
+public class DeleteNodeBST {
+    public static void main(String[] args) {
+        TreeBuilder treeBuilder = new TreeBuilder();
+        TreeBuilder.TreeNode treeNode = treeBuilder.buildTree();
+        TreeBuilder.TreeNode root = treeBuilder.deleteNode(treeNode, 3);
+        System.out.println(root.val);
+    }
+}
