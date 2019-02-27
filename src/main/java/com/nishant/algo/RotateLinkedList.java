77c2872ac71939ce8e25a2d69f73e1198b0c7c23
nishantbhb <nishantbhb@gmail.com>
2019-02-27 10:21:32 +0530

    rotate linked list

---
+++ src/main/java/com/nishant/algo/RotateLinkedList.java
@@ -0,0 +1,48 @@
+package com.nishant.algo;
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+
+public class RotateLinkedList {
+    static ListNode rotateRight(ListNode head, int k) {
+        if (k == 0 || head == null)
+            return head;
+        int ctr = 1;
+        ListNode last = head;
+        while (last.next != null) {
+            last = last.next;
+            ctr++;
+        }
+        k %= ctr;
+        k = ctr - k; //for right rotation
+        while (k > 0) {
+            last.next = head;
+            head = head.next;
+            last = last.next;
+            last.next = null;
+            k--;
+        }
+        return head;
+
+    }
+
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        ListNode newHead = rotateRight(head, 2);
+
+    }
+
+}
