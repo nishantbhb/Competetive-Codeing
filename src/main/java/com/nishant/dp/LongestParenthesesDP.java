ee1fabd2072cf0f68f9b960a26c9b980b870a595
nishantbhb <nishantbhb@gmail.com>
2019-03-28 13:12:02 +0530

    longest valid parentheses

--- src/main/java/com/nishant/dp/LongestParenthesesDP.java
+++ src/main/java/com/nishant/dp/LongestParenthesesDP.java
@@ -11,7 +11,7 @@ class LongestParenthesesDP {
     for (int i = 0; i < s.length(); i++) {
       if (s.charAt(i) == '(') {
         left++;
-      } else if (left > 0) {
+      } else if (left > 0) {// considering the string only has '(' and ')'
         dp[i] = dp[i - 1] + 2;
         dp[i] += (i - dp[i]) >= 0 ? dp[i - dp[i]] : 0;
         max = Math.max(max, dp[i]);
