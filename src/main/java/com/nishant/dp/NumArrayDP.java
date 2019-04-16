e894c059cdeaf06d51a8f3b5e8a59187c7c44aa4
nishantbhb <nishantbhb@gmail.com>
2019-04-16 10:44:11 +0530

    num array dp

---
+++ src/main/java/com/nishant/dp/NumArrayDP.java
@@ -0,0 +1,21 @@
+package com.nishant.dp;
+
+
+class NumArrayDP {
+
+
+    private int[] sums;
+
+    public NumArrayDP(int[] nums) {
+        sums = new int[nums.length + 1];
+        for (int i = 1; i <= nums.length; i++) {
+            sums[i] = sums[i - 1] + nums[i - 1];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sums[j + 1] - sums[i];
+    }
+
+
+}
