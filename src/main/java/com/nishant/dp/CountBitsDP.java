6752f76e3eb344aa651cc887b1465eec0ac8b168
nishantbhb <nishantbhb@gmail.com>
2019-04-16 22:37:39 +0530

    count bits DP

---
+++ src/main/java/com/nishant/dp/CountBitsDP.java
@@ -0,0 +1,18 @@
+package com.nishant.dp;
+
+
+class CountBitsDP {
+
+    public int[] countBits(int num) {
+        int[] bits = new int[num + 1];
+        for (int i = 1; i <= num; i++) {
+            if (i % 2 == 0)
+                bits[i] = bits[i / 2];
+            else
+                bits[i] = bits[i / 2] + 1;
+        }
+        return bits;
+    }
+
+
+}
