a8ca3bba83d2764b7ecbddef8c9951811be296e4
nishantbhb <nishantbhb@gmail.com>
2019-05-12 13:05:14 +0530

    Increasing Decreasing Seq

--- src/main/java/com/nishant/algo/IncreasingDecreasingSeq.java
+++ src/main/java/com/nishant/algo/IncreasingDecreasingSeq.java
@@ -3,8 +3,26 @@ package com.nishant.algo;
 import java.util.Arrays;
 
 public class IncreasingDecreasingSeq {
+    private static int[] diStringMatchOtherWay(String S) {
+        int n = S.length();
+        int[] arr = new int[n + 1];
+        int min = 0;
+        int max = n;
+        for (int i = 0; i < n; i++) {
+            if (S.charAt(i) == 'I') {
+                arr[i] = min;
+                min++;
+            } else {
+                arr[i] = max;
+                max--;
+            }
+        }
 
-    public static int[] diStringMatch(String S) {
+        arr[n] = max; //last element will be the only value left (min=max)
+        return arr;
+    }
+
+    private static int[] diStringMatch(String S) {
         int n = S.length();
         int[] arr = new int[n + 1];
         for (int i = 0; i < n; i++) {
@@ -26,6 +44,7 @@ public class IncreasingDecreasingSeq {
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(diStringMatch("IDID")));
+        System.out.println(Arrays.toString(diStringMatchOtherWay("IDID")));
     }
 
 }
