5e887a73cdd60901e625bd76f638de008bfe1b2f
nishantbhb <nishantbhb@gmail.com>
2019-06-23 17:36:16 +0530

    can Complete Circuit

--- src/main/java/com/nishant/algo/CanCompleteCircuit.java
+++ src/main/java/com/nishant/algo/CanCompleteCircuit.java
@@ -7,7 +7,35 @@ import java.util.List;
 //Google
 public class CanCompleteCircuit {
 
+    //the idea is based in if sum(gas) - sum(cost) >= 0  then there is a solution
+    // restart the tank wherever currTank reaches less than 0
+    public int canCompleteCircuitLeetCode(int[] gas, int[] cost) {
+        int g = gas.length;
+        int c = cost.length;
 
+        if (g != c)
+            return -1;
+
+        int tank = 0;
+        int currTank = 0;
+        int start = 0;
+
+        for (int i = 0; i < g; i++) {
+
+            int currDiff = gas[i] - cost[i];
+            currTank += currDiff;
+            tank += currDiff;
+
+            if (currTank < 0) {
+                currTank = 0;
+                start = i + 1;
+            }
+        }
+        return tank >= 0 ? start : -1;
+    }
+
+
+    //O(n^2)
     public int canCompleteCircuit(final List<Integer> gas, final List<Integer> cost) {
 
         if (gas.size() != cost.size())
