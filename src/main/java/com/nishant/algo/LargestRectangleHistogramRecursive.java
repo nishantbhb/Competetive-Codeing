5dd3dd490651207683e920408f86e7388952e716
nishantbhb <nishantbhb@gmail.com>
2019-06-30 14:34:01 +0530

    largest rectangle in histogram recursive

--- src/main/java/com/nishant/algo/LargestRectangleHistogramRecursive.java
+++ src/main/java/com/nishant/algo/LargestRectangleHistogramRecursive.java
@@ -2,27 +2,37 @@ package com.nishant.algo;
 
 public class LargestRectangleHistogramRecursive {
 
-    public static int largestRectangleArea(int[] heights) {
+    private static int largestRectangleArea(int[] heights) {
         if (heights == null || heights.length == 0)
             return 0;
 
+        //utility function to divide and conquer
         return maxRectangle(heights, 0, heights.length - 1);
     }
 
     private static int maxRectangle(int[] arr, int l, int r) {
 
+        //check if current array is out of bounds
         if (l > r)
             return 0;
 
+        //check if it has only one element then that bar is the max area
         if (l == r)
             return arr[l];
 
+        //find the minimum index in the current range
         int minIndex = findMinIndex(arr, l, r);
 
+        //recursively find max area in the left sub-array
         int left = maxRectangle(arr, l, minIndex - 1);
+
+        //recursively find max area in the right sub-array
         int right = maxRectangle(arr, minIndex + 1, r);
 
+        //find max area among left and right sub-arrays
         int max = Math.max(left, right);
+
+        //compare max with min_bar * curr_range
         return Math.max(max, arr[minIndex] * (r - l + 1));
     }
 
@@ -40,7 +50,8 @@ public class LargestRectangleHistogramRecursive {
 
 
     public static void main(String[] args) {
-        System.out.println(largestRectangleArea(new int[]{2, 1, 2}));
+
+        System.out.println(largestRectangleArea(new int[]{6, 1, 5, 4, 5, 2, 6}));
     }
 
 }
