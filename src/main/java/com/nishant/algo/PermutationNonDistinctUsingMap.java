70af5bfa5dd1b897c9bf2bb7a8f92331e0c34c00
nishantbhb <nishantbhb@gmail.com>
2019-08-18 16:17:09 +0530

    permutation non distinct using map

---
+++ src/main/java/com/nishant/algo/PermutationNonDistinctUsingMap.java
@@ -0,0 +1,57 @@
+package com.nishant.algo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class PermutationNonDistinctUsingMap {
+
+
+    private static List<String> permute(String s) {
+        if (s == null)
+            return null;
+
+        Map<Character, Integer> charMap = buildCharMap(s);
+        List<String> permutes = new ArrayList<>();
+        buildPermute(charMap, "", s.length(), permutes);
+        return permutes;
+
+    }
+
+    private static void buildPermute(Map<Character, Integer> charMap, String prefix, int rem, List<String> permutes) {
+        if (rem == 0) {
+            permutes.add(prefix);
+            return;
+        }
+
+        for (char c : charMap.keySet()) {
+            int cnt = charMap.get(c);
+            if (cnt > 0) {
+                charMap.put(c, cnt - 1);
+                buildPermute(charMap, prefix + c, rem - 1, permutes);
+                charMap.put(c, cnt);
+            }
+        }
+
+    }
+
+    private static Map<Character, Integer> buildCharMap(String s) {
+        if (s == null || s.length() == 0)
+            return new HashMap<>();
+
+        Map<Character, Integer> charMap = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            charMap.put(c, charMap.getOrDefault(c, 0) + 1);
+        }
+        return charMap;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(permute("abc"));
+    }
+
+
+}
+
+
