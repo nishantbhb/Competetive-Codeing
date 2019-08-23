c590675fbd6566ffb1496fb5b3397ee2dc195aff
nishantbhb <nishantbhb@gmail.com>
2019-08-23 10:44:29 +0530

    build parentheses optimal/non-optimal

--- src/main/java/com/nishant/algo/BuildParentheses.java
+++ src/main/java/com/nishant/algo/BuildParentheses.java
@@ -1,7 +1,9 @@
 package com.nishant.algo;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class BuildParentheses {
 
@@ -21,9 +23,37 @@ public class BuildParentheses {
         }
     }
 
+
+    //unoptimized
+    private static Set<String> buildParen(int rem) {
+        if (rem == 0) {
+            Set<String> paren = new HashSet<>();
+            paren.add("");
+            return paren;
+        }
+
+        Set<String> paren = buildParen(rem - 1);
+        Set<String> newParen = new HashSet<>();
+        for (String str : paren) {
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == '(') {
+                    String newSeq = str.substring(0, i + 1) + "()" + str.substring(i + 1);
+                    newParen.add(newSeq);
+                }
+            }
+            newParen.add(str + "()");
+        }
+        return newParen;
+    }
+
+
+
     public static void main(String[] args) {
         List<String> paren = new ArrayList<>();
         buildParen(3, 3, paren, 0, new char[6]);
         System.out.println(paren);
+
+        //unoptimized
+        System.out.println(buildParen(3));
     }
 }
